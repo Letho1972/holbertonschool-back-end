8dd80fef1f029b846e231ffd3918ad1caef2dce1
Letho1972 <ericmarescq@gmail.com>
2023-12-18 13:07:16 +0100

    Gather data from an AP changes code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,8 +9,8 @@ import sys
 
 def get_employee_todo_progress(employee_id):
     base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
+    user_url = "{}/users/{}".format(base_url, employee_id)
+    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
 
     try:
         # Fetch employee information
@@ -23,23 +23,21 @@ def get_employee_todo_progress(employee_id):
         todos_data = todos_response.json()
 
         # Filter completed tasks
-        completed_tasks = [task for task in todos_data if task["completed"]]
+        completed_tasks = [task['title'] for task in todos_data if task["completed"]]
 
         # Display TODO list progress
         total_tasks = len(todos_data)
         completed_tasks_count = len(completed_tasks)
 
-        print(f"Employee {employee_name} is done with tasks\
-               ({completed_tasks_count}/{total_tasks}):")
-        print(f"\t{employee_name}: {completed_tasks_count}\
-               completed tasks out of {total_tasks}")
-
+        print("Employee {} is done with tasks ({}/{}):"\
+              .format(employee_name, completed_tasks_count, total_tasks))
+        
         # Display titles of completed tasks
-        for task in completed_tasks:
-            print(f"\t\t{task['title']}")
+        for task_title in completed_tasks:
+            print("    {}".format(task_title))
 
     except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
+        print("Error fetching data: {}".format(e))
 
 
 if __name__ == "__main__":
